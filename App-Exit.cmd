7a237a32eefd042ba06c0aa5e22919ec2aca3f15
Gitea <gitea@fake.local>
2021-04-28 10:46:32 +0500

    1333

--- App-Exit.cmd
+++ App-Exit.cmd
@@ -1,3 +1,5 @@
 
-
 taskkill /F /IM "Open Server.exe"
+
+nssm start TeamPRO_Nginx
+nssm start TeamPRO_PHP7
--- App-Run.cmd
+++ App-Run.cmd
@@ -1,2 +1,5 @@
 
+nssm stop TeamPRO_Nginx
+nssm stop TeamPRO_PHP7
+
 "d:\Develop\Projects\panels\Open Server.exe"
