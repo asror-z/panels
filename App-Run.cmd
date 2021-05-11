97d509a11333102ab83ba95913a4e8acb23c3ad4
Gitea <gitea@fake.local>
2021-05-11 14:13:25 +0500

    1333333

--- App-Run.cmd
+++ App-Run.cmd
@@ -1,7 +1,11 @@
 
 taskkill /F /IM "Open Server.exe"
 
+FOR %%G IN (00,01,02,03,04,05,06,07,08,09,10,11,12,13,14) DO (
+	Set ServiceName=TeamPRO_PHP7_%%G
+	nssm stop %ServiceName%
+)
+
 nssm stop TeamPRO_Nginx
-nssm stop TeamPRO_PHP7
 
 "d:\Develop\Projects\panels\Open Server.exe"
